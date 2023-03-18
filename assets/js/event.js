a1bd1fd3729c802e8c237748fb4bb5a89b58064b
pedrobolfute <pepenike-101@hotmail.com>
2023-03-18 15:13:40 -0300

    stage

---
+++ assets/js/event.js
@@ -0,0 +1,18 @@
+const main = document.querySelector('.main');
+
+const div = document.createElement('div');
+const svg = document.createElement('svg');
+const span = document.createElement('span')
+
+
+const chart = div;
+chart.classList.add('chart');
+main.appendChild(chart);
+
+const svgCategory1 = svg;
+svgCategory1.classList.add('category-1 points');
+svgCategory1.setAttribute('viewBox', '0 0 260 80');
+svgCategory1.setAttribute('preserveAspectRatio', 'none');
+chart.appendChild(svgCategory1);
+
+const svgCategory2 = div.classList.add('opa');
